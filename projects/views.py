8a80709de00aa6aef845c948b035536a7486a41d
Tearnote <marach5@gmail.com>
2023-05-10 12:46:07 +0100

    Show projects on dashboard

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from . import models
 
 
 def dashboard(request):
     """Render the user/owner dashboard"""
 
-    return render(request, "projects/dashboard.html")
+    # Retrieve all projects
+    projects = models.Project.objects.all()
+
+    context = {
+        'projects': projects,
+    }
+    return render(request, "projects/dashboard.html", context)
