85db349708c6c6b0dd44678e4be8dbbf991cadf7
Tearnote <marach5@gmail.com>
2023-05-10 14:25:20 +0100

    Add authentication checks to dashboard view

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from . import models
 
 
+@login_required
 def dashboard(request):
     """Render the user/owner dashboard"""
 
-    # Retrieve all projects
-    projects = models.Project.objects.all()
+    # Check if user is the website owner
+    is_owner = request.user.is_staff
+
+    # Retrieve all projects for website owner,
+    # or only owned projects for regular user
+    if is_owner:
+        projects = models.Project.objects.all()
+    else:
+        projects = models.Project.objects.filter(user=request.user)
 
     context = {
         'projects': projects,
+        'is_owner': is_owner,
     }
     return render(request, "projects/dashboard.html", context)
