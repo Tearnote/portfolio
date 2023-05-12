7bc6e1d0ad607cc104cbb32741dc5c5a7165d8aa
Tearnote <marach5@gmail.com>
2023-05-12 11:47:50 +0100

    Remove redundant text from testimonial str dunder method

--- projects/models.py
+++ projects/models.py
@@ -50,4 +50,4 @@ class Testimonial(models.Model):
     body = models.TextField(max_length=2048)
 
     def __str__(self):
-        return f"{self.project.user}'s {self.project} testimonial"
+        return f"{self.project} testimonial"
