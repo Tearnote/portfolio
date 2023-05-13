e9df9dee261bb7c2af8b90396dee7aa7345a30f6
Tearnote <marach5@gmail.com>
2023-05-13 18:49:11 +0100

    Create Payment Intent on server

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,8 @@
+import stripe
 from django.http import HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404
 
 from projects import models
-
 
 def checkout(request, project_id):
     """Render the checkout page
@@ -13,7 +13,17 @@ def checkout(request, project_id):
     if project.status != models.Project.PAYABLE:
         return HttpResponseBadRequest()
 
+    # Create the Payment Intent
+    intent = stripe.PaymentIntent.create(
+        amount=project.quote_amount * 100,
+        currency='eur',
+        automatic_payment_methods={
+            'enabled': True,
+        },
+    )
+
     context = {
         'project': project,
+        'client_secret': intent['client_secret']
     }
     return render(request, 'checkout/checkout.html', context)
