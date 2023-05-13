059bb7add9131fe6693a3de3b459ea49940e237d
Tearnote <marach5@gmail.com>
2023-05-13 19:20:01 +0100

    Retrieve Stripe client secret from server via AJAX

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    path('create_payment_intent/', views.create_payment_intent, name='create_payment_intent'),
     path('<int:project_id>/', views.checkout, name='checkout'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,23 @@
 import stripe
-from django.http import HttpResponseBadRequest
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST, require_safe
 
+import portfolio.settings
 from projects import models
 
-def checkout(request, project_id):
-    """Render the checkout page
+
+@login_required
+@require_POST
+def create_payment_intent(request):
+    """Create a Stripe Payment Intent for a project
+    in a payable state
     """
 
     # Retrieve the payable project
-    project = get_object_or_404(models.Project, pk=project_id)
-    if project.status != models.Project.PAYABLE:
+    project = get_object_or_404(models.Project, pk=request.POST['project_id'])
+    if project.status != models.Project.PAYABLE or project.user != request.user:
         return HttpResponseBadRequest()
 
     # Create the Payment Intent
@@ -22,8 +29,24 @@ def checkout(request, project_id):
         },
     )
 
+    return JsonResponse({
+        'clientSecret': intent['client_secret'],
+    })
+
+
+@login_required
+@require_safe
+def checkout(request, project_id):
+    """Render the checkout page
+    """
+
+    # Retrieve the payable project
+    project = get_object_or_404(models.Project, pk=project_id)
+    if project.status != models.Project.PAYABLE or project.user != request.user:
+        return HttpResponseBadRequest()
+
     context = {
         'project': project,
-        'client_secret': intent['client_secret']
+        'stripe_publishable_key': portfolio.settings.STRIPE_PUBLISHABLE_KEY,
     }
     return render(request, 'checkout/checkout.html', context)
