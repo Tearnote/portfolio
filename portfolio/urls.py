772f0600eb2e1a623061903f9eed93ea33ea2bbc
Tearnote <marach5@gmail.com>
2023-05-13 18:30:55 +0100

    Introduce checkout app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('allauth.urls')),
     path('projects/', include('projects.urls')),
+    path('checkout/', include('checkout.urls')),
     path('profile/', include('userprofile.urls')),
     path('', include('home.urls')),
 ]
