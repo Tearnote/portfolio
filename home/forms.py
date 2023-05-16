bebcd9cbebd63c8132efd88300a31711aac41d62
Tearnote <marach5@gmail.com>
2023-05-16 01:21:12 +0100

    Send an email from the contact form

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 
 class ContactForm(forms.Form):
@@ -20,4 +23,24 @@ class ContactForm(forms.Form):
     def save(self):
         """Send an email to website owner
         """
-        pass  # TODO
+
+        if not self.is_valid():
+            return False
+
+        email_context = {
+            'name': self.cleaned_data['name'],
+            'sender': self.cleaned_data['sender'],
+            'project_uuid': self.cleaned_data['project_uuid'],
+            'message': self.cleaned_data['message'],
+        }
+        send_mail(
+            render_to_string(
+                'home/email/contact_subject.txt',
+                email_context, None
+            ).strip(),
+            render_to_string(
+                'home/email/contact_message.txt',
+                email_context, None
+            ),
+            None, [owner.email for owner in User.objects.filter(is_staff=True)]
+        )
