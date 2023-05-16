816b28c84ef5923f02e67b5008a5f0c5f1a7f12e
Tearnote <marach5@gmail.com>
2023-05-16 05:05:22 +0100

    Add example database config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,6 +82,18 @@ DATABASES = {
     }
 }
 
+# Example configuration for Postgres:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'portfolio',
+#         'USER': 'portfolio',
+#         'PASSWORD': os.environ['PORTFOLIO_POSTGRES_PASSWORD'],
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
